c56f459d88cbfc103cad627cd71950e071a58a2f
mike.zhang <mike.zhang@moneycorp.com>
2018-07-03 16:05:56 +0100

    remove unused using

--- NexusTravel.WebAPI/Controllers/FlightSearchController.cs
+++ NexusTravel.WebAPI/Controllers/FlightSearchController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using NexusTravel.Dal.Enums;
+﻿using Microsoft.AspNetCore.Mvc;
 using NexusTravel.Dal.Models;
 
 namespace NexusTravel.WebAPI.Controllers
