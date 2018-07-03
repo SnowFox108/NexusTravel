602aa29d12bd4f573971b274016ce7ed137200b5
mike.zhang <mike.zhang@moneycorp.com>
2018-07-03 15:29:34 +0100

    added API endpoint

---
+++ NexusTravel.WebAPI/Controllers/FlightSearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NexusTravel.Dal.Enums;
+using NexusTravel.Dal.Models;
+
+namespace NexusTravel.WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/FlightSearch")]
+    public class FlightSearchController : Controller
+    {
+        public FlightSearchController()
+        {
+            
+        }
+
+        [HttpPost]
+        public string Post([FromBody] FlightSearchForm form)
+        {
+            return "result";
+        }
+    }
+}
